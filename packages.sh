5c2673594b55125f2fac7db2b57fca489b8329a7
usr-terio <leoteriod@outlook.com>
2026-04-21 20:35:27 -0400

    Add root checks and improve package installation process
    Added root check to ensure script is executed with appropriate permissions. Enhanced package installation process with additional software and configuration.

---
+++ packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Must run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root (use sudo)" 
+   exit 1
+fi
+
+pacman-key --recv-keys 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35 && \
+pacman-key --lsign-key 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35 && \
+echo -e "\n[g14]\nServer = https://arch.asus-linux.org" >> /etc/pacman.conf && \
+pacman -Suy --noconfirm && \
+pacman -S --noconfirm linux-g14 linux-g14-headers nvidia-open-dkms nvidia-utils && \
+pacman -S --noconfirm asusctl supergfxctl rog-control-center power-profiles-daemon && \
+systemctl enable --now power-profiles-daemon supergfxd && \
+pacman -S --noconfirm steam gamemode gamescope fuse2 fwupd fwupd-docs sbctl efitools pacman-contrib unrar unzip ufw gwenview firefox blender bitwarden openh264 terminus-font evolution evince btrfs-assistant apparmor dnscrypt-proxy && \
+pacman -S --needed base-devel --noconfirm && \
+pacman -Rsn htop vim  # Fixed: -Rsn removes package + dependencies
